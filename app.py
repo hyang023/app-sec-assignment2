e581da09fcaf20c20e945a0dd46cf47f656ca950
hyang023 <hwy227@nyu.edu>
2019-10-12 22:34:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-#import flask
 from flask import Flask
-#create an instance of this class. The first argument is the name of the application’s module or package. If you are using a single module (as in this example), you should use __name__ because depending on if it’s started as application or imported as module the name will be different ('__main__' versus the actual import name). This is needed so that Flask knows where to look for templates, static files, and so on
 app = Flask(__name__)
 
-#the route() decorator tells Flask what URL should trigger our function
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-#citation: http://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
