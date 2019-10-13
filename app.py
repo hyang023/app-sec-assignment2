133ca4b2774d1b8a9d9e8b41f2c9e2ff544ba209
hyang023 <hwy227@nyu.edu>
2019-10-12 22:39:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/register')
+def hello_world():
+    return 'registration page'
+
+@app.route('/login')
+def hello_world():
+    return 'login page'
+
+@app.route('/spell_check')
+def hello_world():
+    return 'spell_check page'
