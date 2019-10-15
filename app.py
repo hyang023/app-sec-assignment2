c7de98e9c1601229c220ab87d8011bf87ce3554a
hyang023 <hwy227@nyu.edu>
2019-10-15 12:14:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,31 +7,31 @@ def hello_world():
 
 @app.route('/register', methods=['post', 'get'])
 def register():
-	message = ''
-	if request.method == 'POST':
-            uname = request.form.get('uname')
-            pword = request.form.get('pword')
-            twofa = request.form.get('2fa')
+    message = ''
+    if request.method == 'POST':
+        uname = request.form.get('uname')
+        pword = request.form.get('pword')
+        twofa = request.form.get('2fa')
  
-            if uname  and pword :
-                message = "Success"
+        if uname  and pword :
+            message = "Success"
     return render_template('registration.html', message=message)
 
 @app.route('/login',  methods=['post', 'get'])
 def login():
-	message = ''
-	if request.method == 'POST':
-            uname = request.form.get('uname')
-            pword = request.form.get('pword')
-            2fa = request.form.get('2fa')
+    message = ''
+    if request.method == 'POST':
+        uname = request.form.get('uname')
+        pword = request.form.get('pword')
+        2fa = request.form.get('2fa')
  
-            if uname  and pword :
-                message = "Success"
+        if uname  and pword :
+            message = "Success"
     return render_template('login.html', message=message)
 
 @app.route('/login_success', methods=['POST'])
 def login_success():
-	return render_template('login_success.html')
+    return render_template('login_success.html')
 
 @app.route('/spell_check')
 def spell_check():
