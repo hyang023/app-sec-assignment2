3cc2f529869dc0234b605b783e7e64d147a40f36
hyang023 <hwy227@nyu.edu>
2019-10-15 12:10:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,32 +9,24 @@ def hello_world():
 def register():
 	message = ''
 	if request.method == 'POST':
-        uname = request.form.get('uname')
-        pword = request.form.get('pword')
-        2fa = request.form.get('2fa')
+            uname = request.form.get('uname')
+            pword = request.form.get('pword')
+            twofa = request.form.get('2fa')
  
-        if uname  and pword :
-        #    message = "Correct username and password"
-        #else:
-            message = "Success"
+            if uname  and pword :
+                message = "Success"
     return render_template('registration.html', message=message)
-
-@app.route('/register_success', methods=['POST'])
-def register_success():
-	return render_template('register_success.html')
 
 @app.route('/login',  methods=['post', 'get'])
 def login():
 	message = ''
 	if request.method == 'POST':
-        uname = request.form.get('uname')
-        pword = request.form.get('pword')
-        2fa = request.form.get('2fa')
+            uname = request.form.get('uname')
+            pword = request.form.get('pword')
+            2fa = request.form.get('2fa')
  
-        if uname  and pword :
-        #    message = "Correct username and password"
-        #else:
-            message = "Success"
+            if uname  and pword :
+                message = "Success"
     return render_template('login.html', message=message)
 
 @app.route('/login_success', methods=['POST'])
