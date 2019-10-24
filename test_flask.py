c59ef9fdccba8678fdc620eb0f67ca270eec17be
hyang023 <hwy227@nyu.edu>
2019-10-24 16:36:44 -0400

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -3,8 +3,3 @@ import pytest
 import app
 
 @pytest.fixture
-def test_hello_world(app):
-    res = app.get("/")
-    # print(dir(res), res.status_code)
-    assert res.status_code == 200
-    assert b"Hello World" in res.data
