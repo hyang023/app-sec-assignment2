9fb1c9852b7291e723a7b1727de51256e7bcaffe
hyang023 <hwy227@nyu.edu>
2019-10-25 23:19:04 -0400

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -3,7 +3,7 @@ import pytest
 import app
 
 @pytest.fixture
-def app():
-    app = app.create_app()
-    app.debug = True
-    return app.test_client()
+def apptest():
+    apptest = app.create_app()
+    apptest.debug = True
+    return apptest.test_client()
