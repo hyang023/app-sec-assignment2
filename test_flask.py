b481cde7f20c116fe9a8198f2efb31f0d9a0fceb
hyang023 <hwy227@nyu.edu>
2019-10-25 23:33:05 -0400

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -8,8 +8,8 @@ def apptest():
     apptest.debug = True
     return apptest.test_client()
 
-def test_hello_world(app):
-    res = app.get("/")
+def test_hello_world(apptest):
+    res = apptest.get("/")
     # print(dir(res), res.status_code)
     assert res.status_code == 200
     assert b"Hello World" in res.data
